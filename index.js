162573a2be86ecba030f1fb3e075d6111ceb11fd
sabilimaulana <spacysbl.learn@gmail.com>
2021-07-24 15:16:04 +0700

    try3

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ app.use("/", (req, res) => {
   res.send("<marquee><h1>Sebuah backend oleh sabilimaulana</h1></marquee>");
 });
 
-db.sequelize.sync().then((req) => {
-  app.listen(PORT, () => {
-    console.log(`App running on localhost on PORT ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`App running on localhost on PORT ${PORT}`);
 });
+
+// db.sequelize.sync().then((req) => {
+//   app.listen(PORT, () => {
+//     console.log(`App running on localhost on PORT ${PORT}`);
+//   });
+// });
